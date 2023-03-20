ef38d25114f984c1da38a354d2bef737fbff6026
sharad2512 <shnarute95@gmail.com>
2023-03-20 12:24:07 +0530

    ADDED : Code to find maximum string from three string

--- src/com/generics/FindMaximumGenerics.java
+++ src/com/generics/FindMaximumGenerics.java
@@ -19,8 +19,22 @@ public class FindMaximumGenerics {
             return c;
         }
     }
+    public static String findMaxString(String x, String y, String z){
+        int a = x.length();
+        int b = y.length();
+        int c = z.length();
+        System.out.println("a:"+a+" b:"+b+" c:"+c);
+        if (a > b && a > c){
+            return x;
+        } else if (b > c) {
+            return y;
+        }else{
+            return z;
+        }
+    }
     public static void main(String[] args) {
         System.out.println("Maximum Integer value is :"+ FindMaximumGenerics.findMaxInt(12,13,15));
-        System.out.println("Maximum Integer value is :"+ FindMaximumGenerics.findMaxFloat(12.12f,13.33f,15.22f));
+        System.out.println("Maximum Float value is :"+ FindMaximumGenerics.findMaxFloat(12.12f,13.33f,15.22f));
+        System.out.println("Maximum String value is :"+ FindMaximumGenerics.findMaxString("Sharad.","Narute","Pune"));
     }
 }
