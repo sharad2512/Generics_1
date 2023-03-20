f6ebe7d6cd1a809b8b0e5f8fe9e2345911877eb3
sharad2512 <shnarute95@gmail.com>
2023-03-20 15:35:50 +0530

    ADDED Code to refactor-1

--- src/com/generics/FindMaximumGenerics.java
+++ src/com/generics/FindMaximumGenerics.java
@@ -1,40 +1,18 @@
 package com.generics;
 
 public class FindMaximumGenerics {
-    public static Integer findMaxInt(Integer a, Integer b, Integer c){
-        if (a > b && a > c){
-            return a;
-        } else if (b > c ) {
-            return b;
-        }else {
-            return c;
+    public static <T extends Comparable<T>> T findMaxVal(T a, T b, T c) {
+        T max = a;
+        if (b.compareTo(max) > 0) {
+            max = b;
+        }if (c.compareTo(max) > 0) {
+            max = c;
         }
-    }
-    public static Float findMaxFloat(Float a, Float b, Float c){
-        if (a > b && b > c){
-            return a;
-        } else if (b > c) {
-            return b;
-        }else {
-            return c;
-        }
-    }
-    public static String findMaxString(String x, String y, String z){
-        int a = x.length();
-        int b = y.length();
-        int c = z.length();
-        System.out.println("a:"+a+" b:"+b+" c:"+c);
-        if (a > b && a > c){
-            return x;
-        } else if (b > c) {
-            return y;
-        }else{
-            return z;
-        }
+            return max;
     }
     public static void main(String[] args) {
-        System.out.println("Maximum Integer value is :"+ FindMaximumGenerics.findMaxInt(12,13,15));
-        System.out.println("Maximum Float value is :"+ FindMaximumGenerics.findMaxFloat(12.12f,13.33f,15.22f));
-        System.out.println("Maximum String value is :"+ FindMaximumGenerics.findMaxString("Sharad.","Narute","Pune"));
+        System.out.println("Max from Integer :" + FindMaximumGenerics.findMaxVal(1, 3, 55));
+        System.out.println("Max from Float :" + FindMaximumGenerics.findMaxVal(1.2f, 1.44f, 1.76f));
+        System.out.println("Max from String :" + FindMaximumGenerics.findMaxVal("Apple", "Mango", "Banana"));
     }
 }
