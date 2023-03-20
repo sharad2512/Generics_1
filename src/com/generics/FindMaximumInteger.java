95a3c93bc7e0643538ea44a2e776aab236212e4f
sharad2512 <shnarute95@gmail.com>
2023-03-20 11:57:27 +0530

    ADDED : code to the find maximum from three integer

--- src/com/generics/FindMaximumInteger.java
+++ src/com/generics/FindMaximumInteger.java
@@ -1,7 +1,16 @@
 package com.generics;
 
 public class FindMaximumInteger {
+    public static Integer compareTo(Integer a, Integer b, Integer c){
+        if (a > b && a > c){
+            return a;
+        } else if (b > c ) {
+            return b;
+        }else {
+            return c;
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("Welcome to Generics programing");
+        System.out.println("Maximum Integer value is :"+FindMaximumInteger.compareTo(12,13,15));
     }
 }
